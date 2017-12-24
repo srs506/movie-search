0bde2cece6d42381caea3caa988ad88b23b1d965
Sam Scheffler <schefflersam@gmail.com>
2017-12-24 12:43:09 -0800

    Fix broken import

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import MovieTable from '../Table';
+import MovieTable from '../MovieTable';
 import './index.css';
 
 const PATH_BASE = `https://api.themoviedb.org/3`;
