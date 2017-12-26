60325ff2dce8652f194ecf76db7ec21c24a851e9
Sam Scheffler <schefflersam@gmail.com>
2017-12-26 13:07:22 -0800

    Add snapshot test

--- src/components/App/test.js
+++ src/components/App/test.js
@@ -1,8 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import App from './index';
+import renderer from 'react-test-renderer';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
+});
+
+test('has a valid snapshot', () => {
+  const component = renderer.create(<App />);
+
+  let tree = component.toJSON();
+  expect(tree).toMatchSnapshot();
 });
