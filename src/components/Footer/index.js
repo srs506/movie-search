2a762d8cd403ee0f93084fecd9bbb5d5c74fe68c
Sam Scheffler <schefflersam@gmail.com>
2017-12-28 21:53:11 -0600

    Fix footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,11 +1,12 @@
 import * as React from 'react';
-import TMDBLogo from './TMDBLogo';
+import TMDBLogo from './tmdbLogo.png';
 
 const Footer = props => {
-  return;
-  <div className="footer">
-    <img src={TMDBLogo} height="60px;" alt="Powered by The Movie DB" />
-  </div>;
+  return (
+    <div className="footer">
+      <img src={TMDBLogo} height="60px;" alt="Powered by The Movie DB" />
+    </div>
+  );
 };
 
 export default Footer;
